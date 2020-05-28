87126ba9ec657eb97a722fc16771e9f6d11113ee
unknown <evanshango>
2020-05-29 00:06:03 +0300

    edit proxy and server base URL

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -7,7 +7,7 @@ import {ApolloProvider} from '@apollo/react-hooks'
 import {setContext} from "apollo-link-context";
 
 const httpLink = createHttpLink({
-    uri: 'http://localhost:5000'
+    uri: 'https://blooming-brushlands-07000.herokuapp.com/'
 })
 
 const authLink = setContext(() => {
